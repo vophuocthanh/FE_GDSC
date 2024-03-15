3d32b5deb4a5d62a9eced94ec070739d1eb5b703
anh29 <anhhna17710110@gmail.com>
2024-03-15 10:56:28 +0700

    fix: change images in card

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,12 @@
-let cards = [];
+let cards = [1, 2, 3, 4, 5, 6];
 let num_cards = 6;
 
-while (cards.length < num_cards) {
-  let randomNumber = Math.floor(Math.random() * 11) + 1;
-  if (!cards.includes(randomNumber)) {
-    cards.push(randomNumber);
-  }
-}
+// while (cards.length < num_cards) {
+//   let randomNumber = Math.floor(Math.random() * 5) + 1;
+//   if (!cards.includes(randomNumber)) {
+//     cards.push(randomNumber);
+//   }
+// }
 
 cards = [...cards, ...cards];
 
@@ -37,7 +37,7 @@ function newGame() {
   timer.style.display = "none";
   timeStart = false;
   timer.textContent = minute + " Minutes " + second + " Seconds";
-  // shuffle(cards);
+  shuffle(cards);
   cards_select = [];
   matches = 0;
   moves.textContent = 0;
@@ -105,7 +105,7 @@ function endGame() {
     modalText.innerHTML =
       "<h2>Congratulations!<br /> You made it</h2> <br> Time taken: " +
       minute +
-      "mintes " +
+      "minutes " +
       second +
       "seconds <br> Moves Taken: " +
       movesCount +
@@ -126,6 +126,7 @@ restart.addEventListener("click", function () {
 if (!movesWait) {
   deck.addEventListener("click", function (e) {
     let card = e.target;
+    console.log(card);
 
     if (e.target !== e.currentTarget) {
       if (!timeStart) {
@@ -140,7 +141,10 @@ if (!movesWait) {
         }
         if (cards_select.length === 2) {
           addMove(card);
-          if (cards_select[0].innerHTML === cards_select[1].innerHTML) {
+          if (
+            cards_select[0].querySelector("img").src ===
+            cards_select[1].querySelector("img").src
+          ) {
             cardMatch();
           } else {
             cardMisMatch();
@@ -170,20 +174,20 @@ function startTimer() {
 }
 
 // Shuffle function from http://stackoverflow.com/a/2450976
-// function shuffle(array) {
-//   var currentIndex = array.length,
-//     temporaryValue,
-//     randomIndex;
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
-//   while (currentIndex !== 0) {
-//     randomIndex = Math.floor(Math.random() * currentIndex);
-//     currentIndex -= 1;
-//     temporaryValue = array[currentIndex];
-//     array[currentIndex] = array[randomIndex];
-//     array[randomIndex] = temporaryValue;
-//   }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-//   return array;
-// }
+  return array;
+}
 
 newGame();
